5590030c5a19f3b2dfa5c2751e99c3b9bda12f1e
Your Name <your-email@example.com>
2025-07-22 15:05:07 +0900

    feat:バリデーション処理を追加
    - 予約時のデータを、予約番号からユーザーIDに修正

--- resources/views/reservations/create.blade.php
+++ resources/views/reservations/create.blade.php
@@ -8,8 +8,8 @@
         <form method="post" action="{{ route('reservation.store') }}">
             @csrf
             <div>
-                <label for="user_id">予約番号</label>
-                <input type="text" name="user_id">
+                <label for="user_id">ユーザー番号</label>
+                <input type="number" name="user_id">
 
                 <label for="reservation_datetime">予約日時:</label>
                 <input type="datetime-local" name="reservation_datetime" id="reservation_datetime" required>
