339a65a6f4d938982a56d001ba059d8739bfc4e0
Your Name <your-email@example.com>
2025-07-22 16:39:09 +0900

    style:予約一覧画面のレイアウトを調整
    - テキストを表示しているだけだったため、レイアウトを調整して視認性を向上

--- resources/views/reservations/index.blade.php
+++ resources/views/reservations/index.blade.php
@@ -1,19 +1,35 @@
-こんにちは
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            予約一覧
+        </h2>
+    </x-slot>
 
-<a href="{{ route('reservation.create') }}">予約はこちら</a>
+    <div class="mx-auto px-6">
+        <p>
+            こんにちは
+        </p>
 
+        <a href="{{ route('reservation.create') }}">予約はこちら</a>
 
-@if (session('success'))
-            <div class="alert alert-success">
-                {{ session('success') }}
-            </div>
+        @if (session('success'))
+        <div class="alert alert-success">
+            {{ session('success') }}
+        </div>
         @endif
 
-@foreach ($reservations as $user)
+        @foreach ($reservations as $user)
+        <div class="mt-4 p-8 bg-white w-full rounded-2xl">
+            <h1 class="p-4 text-lg font-semibold">
+                {{ $user->user_id }}
+            </h1>
+            <hr class="w-full">
+            <p class="mt-4 p-4">
+                予約日時:{{ $user->reservation_datetime }}
+            </p>
+        </div>
+        @endforeach
+    </div>
 
-<p>
-    {{ $user->user_id }}
-    {{ $user->reservation_datetime }}
-</p>
-@endforeach
 
+</x-app-layout>
