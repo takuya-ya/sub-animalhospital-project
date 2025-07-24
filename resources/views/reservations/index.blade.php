3a138fd55d37f2cc34b99aa0aba31feb32856f74
Your Name <your-email@example.com>
2025-07-24 13:19:07 +0900

    style:メッセージ出力の調整

--- resources/views/reservations/index.blade.php
+++ resources/views/reservations/index.blade.php
@@ -6,14 +6,14 @@
     </x-slot>
 
     <div class="mx-auto px-6">
-        <p>
+        <p class="mb-4 text-lg">
             こんにちは
         </p>
 
         <a href="{{ route('reservation.create') }}">予約はこちら</a>
 
         @if (session('success'))
-        <div class="alert alert-success">
+        <div class="mt-6 mb-4 p-4 bg-green-100 text-green-800 rounded-lg shadow">
             {{ session('success') }}
         </div>
         @endif
@@ -21,9 +21,9 @@
         <!-- 予約一覧表示 -->
         <!-- $reservations は Reservation モデルのインスタンスの コレクション（配列のようなもの）。controllerから渡す -->
         @foreach ($reservations as $reservation)
-        <div class="mt-4 p-8 bg-white w-full rounded-2xl">
+        <div class="mt-4 p-8 bg-white w-full rounded-2xl shadow">
             <h1 class="p-4 text-lg font-semibold">
-                <a href=" {{ route('reservation.show', $reservation)}}" class="text-blue-600">
+                <a href=" {{ route('reservation.show', $reservation)}}" class="text-blue-600 hover:underline">
                     名前 : {{ $reservation->user->name }} 様&nbsp;&nbsp;/&nbsp;&nbsp;ユーザーID : {{ $reservation->user_id }} 
                 </a>
             </h1>
